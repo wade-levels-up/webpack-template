56042c6cbf64f08388c344a1124e21b202fb96d4
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-10-12 20:52:28 +1100

    Enable broswer environment for ESLint
    ESLint was flagging global variables such as
    'document'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
+  env: {
+    browser: true,
+  },
+  globals: {
+    document: 'readonly',
+  },
   rules: {
     'prettier/prettier': [
       'error',
