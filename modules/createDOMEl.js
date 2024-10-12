b6c4da92a868c247b7876336cb446634c8ff24e6
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-10-12 13:27:04 +1100

    Add eslint-config-prettier to devDep

--- modules/createDOMEl.js
+++ modules/createDOMEl.js
@@ -1,9 +1,22 @@
-export function createDOMElement(tag, textContent, att1, attVal1, att2, attVal2){
-    let element = document.createElement(tag);
-    if (textContent) { element.textContent = textContent };
-    if (att1) { element.setAttribute(att1, attVal1) };
-    if (att2) { element.setAttribute(att2, attVal2) };
-    return element;
+export function createDOMElement(
+  tag,
+  textContent,
+  att1,
+  attVal1,
+  att2,
+  attVal2,
+) {
+  let element = document.createElement(tag);
+  if (textContent) {
+    element.textContent = textContent;
+  }
+  if (att1) {
+    element.setAttribute(att1, attVal1);
+  }
+  if (att2) {
+    element.setAttribute(att2, attVal2);
+  }
+  return element;
 }
 
-export const test = 'DOM Content Loaded! :)';
+export const test = "DOM Content Loaded! :)";
